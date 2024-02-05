31233ee7274d89e17f6b901c74c856e79b738650
inhelzer <121232928+inhelzer@users.noreply.github.com>
2024-02-05 19:09:04 +0200

    prefabs

--- GuitarHero/Assets/Scripts/Destroyer.cs
+++ GuitarHero/Assets/Scripts/Destroyer.cs
@@ -6,6 +6,6 @@ public class Destroyer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
+        Destroy(other.transform.parent.gameObject);
     }
 }
