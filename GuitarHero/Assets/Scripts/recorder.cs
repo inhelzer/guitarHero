d65c98fed32cd862a4c4386d0b93ff556465ec9a
inhelzer <121232928+inhelzer@users.noreply.github.com>
2024-02-05 20:28:26 +0200

    red dress 1

---
+++ GuitarHero/Assets/Scripts/recorder.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class recorder : MonoBehaviour, Controls.IGameControlsActions
+{
+    Controls controls;
+    string a;
+    string b;
+    string c;
+    string d;
+
+    public void OnEnter(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("one:" + a);
+            Debug.Log("two:" + b);
+            Debug.Log("three:" + c);
+            Debug.Log("four:" + d);
+
+        }
+    }
+
+    public void On_1(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            a = a + $"{System.Math.Round(Time.timeSinceLevelLoad, 2)},";
+        }
+    }
+
+    public void On_2(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            b = b + $"{System.Math.Round(Time.timeSinceLevelLoad, 2)},";
+        }
+    }
+
+    public void On_3(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            c = c + $"{System.Math.Round(Time.timeSinceLevelLoad, 2)},";
+        }
+    }
+
+    public void On_4(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            d = d + $"{System.Math.Round(Time.timeSinceLevelLoad, 2)},";
+        }
+    }
+
+    private void Awake()
+    {
+        controls = new Controls();
+        controls.GameControls.SetCallbacks(this);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls.GameControls.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        controls.GameControls.Disable();
+    }
+}
